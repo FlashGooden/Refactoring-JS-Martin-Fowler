75315941c0baa480335d111fd0f30964ec0b4e88
FlashGooden <Flash@heysparkbox.com>
2022-03-18 21:28:57 -0400

    refactor: inline variable for thisAmount + playFor(perf) integration

--- src/Refactor.js
+++ src/Refactor.js
@@ -23,28 +23,6 @@ const bills = {
   ],
 };
 
-const amountFor = (perf, play) => {
-  let thisAmount = 0;
-
-  switch (play.type) {
-    case 'tragedy':
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-      }
-      break;
-    case 'comedy':
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
-      }
-      thisAmount += 300 * perf.audience;
-      break; default:
-      throw new Error(`unknown type: ${play.type}`);
-  }
-
-  return thisAmount;
-};
 
 const statement = (invoice, plays) => {
   let totalAmount = 0;
@@ -60,17 +38,38 @@ const statement = (invoice, plays) => {
     },
   );
 
+  const playFor = (aPerformance) => plays[aPerformance.playID];
+
+  const amountFor = (aPerformance) => {
+    let result = 0;
+
+    switch (playFor(aPerformance).type) {
+      case 'tragedy':
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break; default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+
+    return result;
+  };
+
   for (const perf of invoice.performances) {
-    const play = plays[perf.playID];
-
-    const thisAmount = amountFor(perf, play);
-
     // add volume credits
     // add extra credit for every ten comedy attendees
-    if (plays.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
+    if (playFor(perf).type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
     // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
