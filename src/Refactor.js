3b7577dfddfcb9bfce7fe7fc129c4459c62d8f56
FlashGooden <Flash@heysparkbox.com>
2022-03-19 01:45:16 -0400

    refactor: split loop and extract function volumeCredit

--- src/Refactor.js
+++ src/Refactor.js
@@ -23,58 +23,78 @@ const bills = {
   ],
 };
 
+const playFor = (aPerformance) => playList[aPerformance.playID];
 
-const statement = (invoice, plays) => {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+const amountFor = (aPerformance) => {
+  let result = 0;
 
-  const { format } = new Intl.NumberFormat(
-    'en-US',
-    {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    },
-  );
-
-  const playFor = (aPerformance) => plays[aPerformance.playID];
-
-  const amountFor = (aPerformance) => {
-    let result = 0;
-
-    switch (playFor(aPerformance).type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break; default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }
-
-    return result;
-  };
-
-  for (const perf of invoice.performances) {
-    // add volume credits
-    // add extra credit for every ten comedy attendees
-    if (playFor(perf).type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
-    // print line for this order
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
-    totalAmount += amountFor(perf);
+  switch (playFor(aPerformance).type) {
+    case 'tragedy':
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case 'comedy':
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break; default:
+      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
 
   return result;
 };
 
-console.log(statement(bills, playList));
+const volumeCreditsFor = (aPerformance) => {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
+
+  // add extra credit for every ten comedy attendees
+  if (playFor(aPerformance).type === 'comedy') result += Math.floor(aPerformance.audience / 5);
+
+  return result;
+};
+
+const formatUSD = (aNumber) => new Intl.NumberFormat(
+  'en-US',
+  {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  },
+).format(aNumber / 100);
+
+const totalVolumeCredits = (invoice) => {
+  let volumeCredits = 0;
+
+  for (const perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+
+  return volumeCredits;
+};
+
+const statement = (invoice) => {
+  let totalAmount = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+
+  for (const perf of invoice.performances) {
+    // print line for this order
+    result += ` ${playFor(perf).name}: ${formatUSD(amountFor(perf))} (${perf.audience} seats)\n`;
+  }
+
+  for (const perf of invoice.performances) {
+    totalAmount += amountFor(perf);
+  }
+
+
+  result += `Amount owed is ${formatUSD(totalAmount)}\n`;
+  result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
+
+  return result;
+};
+
+console.log(statement(bills));
